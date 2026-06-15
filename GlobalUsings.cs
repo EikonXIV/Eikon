71ab7c2a92b4cc42eee14b82d0a06d0a10a25779
Rian Morningstar <akos.tattoo.co@gmail.com>
2026-06-14 20:12:00 -0600

    chore: scaffold Dalamud plugin project

---
+++ GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Numerics;
+global using System.Threading.Tasks;
+global using Dalamud.Bindings.ImGui;
+global using OtterGui.Raii;
+global using OtterGui.Widgets;
